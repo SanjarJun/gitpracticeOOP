92a01ede1b8dcf11f06e17fbb16ebeabf51e0f72
Dokja Kim <oneloveshinobu@gmail.com>
2023-04-09 23:34:03 +0600

    new branches

--- src/main/java/com/gitpractice/GitpracticeApplication.java
+++ src/main/java/com/gitpractice/GitpracticeApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitpracticeApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(GitpracticeApplication.class, args);
+		System.out.println("blabla");
 	}
 
 }
